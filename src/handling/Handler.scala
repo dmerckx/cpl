62ff1960a8c37e70b6409931beb33080494f5a24
Kristof Peeters <circafreak@hotmail.com>
2012-12-26 16:19:34 +0100

    implementatie van city operations. Nog niet getest

--- src/handling/Handler.scala
+++ src/handling/Handler.scala
@@ -4,56 +4,236 @@ import syntax.AddAirport
 import syntax.AddCity
 import syntax.Airport_data
 import syntax.City_data
+import syntax.City1
+import syntax.City2
+import syntax.City3
+import syntax.City
 import syntax.Operation
 import syntax.ChangeTemplatePricesTo
+import syntax.ChangeCityName
+import syntax.ChangeCityCode
 import syntax.Template
 import syntax.PricePeriod
+import syntax.RemoveCity
+import handling.exceptions.IllegalCityNameException
+import handling.exceptions.IllegalCityCodeException
+import handling.exceptions.AlreadyExistingCityNameException
+import handling.exceptions.NoSuchCityException
+import com.sun.org.apache.xalan.internal.xsltc.compiler.ForEach
+import handling.exceptions.AlreadyExistingCityShortException
+import handling.exceptions.NotAllowedCityNameException
+import handling.exceptions.NotAllowedCityCodeException
 
-
-case class TemplateException(templ:Template, msg:String) extends Exception;
+case class TemplateException(templ: Template, msg: String) extends Exception;
 
 object Handler {
 
-  def handle(op:Operation) = {
-    op match {
-      case AddCity(data) => addCity(data) 
-      case AddAirport(data) => addAirport(data)
-      //...
-      case ChangeTemplatePricesTo(templ, to) => changeTemplatePricesTo(templ, to)
-    }
-  }
-  
-  // ----- OPERATIONS ----- //
-  
-  def addCity(data:City_data) = {
-	  //Check if the name of this city etc is correct (only a-zA-Z), if not throw error
-	  	
-	  //If all checks are ok, add this city to the database 
-	  val query = "INSERT (...) INTO cities";
-	  println("Add city with query: " + query);
-  }
-  
-  def addAirport(data:Airport_data) = {
-	  //Do query and shizzle
-	  println("data: " + data);
-  }
-  
-  def changeTemplatePricesTo(templ:Template, to:List[PricePeriod]) = {
-	  //Check template..
-	  checkTemplate(templ);
-    
-	  //Check price periods..
-  }
-  
-  
-  // ------ CHECKS ----- //
-  
-  def checkTemplate(templ:Template) = {
-    //Check database to see if this template corresponds to a correct template
-    val query = "SELECT * FROM Templates WHERE flightnr=\"" + templ.flightnr + "\"";
-    println("Check template with query: " + query);  
-    
-    //If no such flightnr can be found..
-    throw new TemplateException(templ, "No template with such flightnr excists");
-  }
+	def handle(op: Operation) = {
+		op match {
+		case AddCity(data) => addCity(data)
+		case ChangeCityName(city, to) => changeCityNameSuper(city, to)
+		case ChangeCityCode(city, to:String) => changeCityCodeSuper(city, to)
+		case RemoveCity(city) => removeCitySuper(city)
+		
+		case AddAirport(data) => addAirport(data)
+		//...
+		case ChangeTemplatePricesTo(templ, to) => changeTemplatePricesTo(templ, to)
+		}
+	}
+
+	// ----- OPERATIONS ----- //
+
+	// ----- Begin Cities ------ //
+
+	/**
+	 * If there is a city that has either the same name or the same code, then the add will not succeed. 
+	 * So each city has to have a unique name and code (the combination is not sufficient).
+	 */
+	def addCity(data: City_data) = {
+		//Check if the properties of this city etc are correct (only a-zA-Z), if not throw error.
+		if (data.name == null || !data.name.matches("[a-zA-Z]+"))
+			throw new IllegalCityNameException(data.name);
+		if (data.short == null || data.short.length() != 3)
+			throw new IllegalCityCodeException(data.short);
+
+		if (evalGet(select("*", "CITY", "name=" + data.name)))
+			throw new AlreadyExistingCityNameException(data.name);
+		if (evalGet(select("*", "CITY", "idCity=" + data.short)))
+			throw new AlreadyExistingCityShortException(data.short);
+
+		//If all checks are ok, add this city to the database 
+		val query = insert("CITY", makeValues(List(data.short, data.name)));
+		executeQuery(query);
+		println("Add city with query: " + query);
+	}
+
+	def makeValues(list: List[Object]): String = {
+		var result = "(";
+		list.foreach(obj =>
+		result = result + "'" + obj.toString() + "',");
+		result = result.substring(0, result.length() - 1) + ")";
+		return result;
+	}
+	//TODO andere naam!
+	def changeCityNameSuper(city: City, to: String) = {
+		city match {
+		case city:City1 => changeCityName(city, to)
+		case city:City2 => changeCityName(city, to)
+		case city:City3 => changeCityName(city, to)
+		}
+	}
+
+	def changeCityName(city: City1, to: String) = {
+		if (!evalGet(select("*", "CITY", "name=" + city.name)))
+			throw new NoSuchCityException(city.name);
+
+		if (evalGet(select("*", "CITY", "name=" + to)))
+			throw new NotAllowedCityNameException(to);
+
+		executeQuery(update("CITY", "name=" + to, "name=" + city.name));
+	}
+
+	def changeCityName(city: City2, to: String) = {
+		if (!evalGet(select("*", "CITY", "idCity=" + city.short)))
+			throw new NoSuchCityException(city.short);
+
+		if (evalGet(select("*", "CITY", "name=" + to)))
+			throw new NotAllowedCityNameException(city.short);
+
+		executeQuery(update("CITY", "name=" + to, "idCity=" + city.short));
+	}
+
+	def changeCityName(city: City3, to: String) = {
+		if (!evalGet(select("*", "CITY", "idCity=" + city.short + " AND " + "name=" + city.name)))
+			throw new NoSuchCityException(city.short);
+
+		if (evalGet(select("*", "CITY", "name=" + to)))
+			throw new NotAllowedCityNameException(city.short);
+
+		executeQuery(update("CITY", "name=" + to, "idCity=" + city.short  + " AND " + "name=" + city.name));
+	}
+
+	def changeCityCodeSuper(city:City, to:String) {
+		city match {
+		case city:City1 => changeCityCode(city, to)
+		case city:City2 => changeCityCode(city, to)
+		case city:City3 => changeCityCode(city, to)
+		}
+	}
+
+	def changeCityCode(city:City1, to:String) {
+		if (!evalGet(select("*", "CITY", "name=" + city.name)))
+			throw new NoSuchCityException(city.name);
+
+		if (evalGet(select("*", "CITY", "idCity=" + to)))
+			throw new NotAllowedCityCodeException(to);
+
+		executeQuery(update("CITY", "idCity=" + to, "name=" + city.name));
+	}
+
+	def changeCityCode(city:City2, to:String) {
+		if (!evalGet(select("*", "CITY", "idCity=" + city.short)))
+			throw new NoSuchCityException(city.short);
+
+		if (evalGet(select("*", "CITY", "idCity=" + to)))
+			throw new NotAllowedCityCodeException(to);
+
+		executeQuery(update("CITY", "idCity=" + to, "idCity=" + city.short));
+	}
+
+	def changeCityCode(city:City3, to:String) {
+		if (!evalGet(select("*", "CITY", "idCity=" + city.short + " AND " + "name=" + city.name)))
+			throw new NoSuchCityException(city.short);
+
+		if (evalGet(select("*", "CITY", "idCity=" + to)))
+			throw new NotAllowedCityCodeException(to);
+
+		executeQuery(update("CITY", "idCity=" + to, "idCity=" + city.short + " AND " + "name=" + city.name));
+	}
+
+	//TODO naam veranderen
+	def removeCitySuper(city:City) {
+		city match {
+		case city:City1 => removeCity(city)
+		case city:City2 => removeCity(city)
+		case city:City3 => removeCity(city)
+		}
+	}
+
+	def removeCity(city:City1) {
+		if (!evalGet(select("*", "CITY", "name=" + city.name)))
+			throw new NoSuchCityException(city.name);
+
+		executeQuery(delete("CITY", "name=" + city.name));
+	}
+
+	def removeCity(city:City2) {
+		if (!evalGet(select("*", "CITY", "idCity=" + city.short)))
+			throw new NoSuchCityException(city.short);
+
+		executeQuery(delete("CITY", "idCity=" + city.short));
+	}
+
+	def removeCity(city:City3) {
+		if (!evalGet(select("*", "CITY", "idCity=" + city.short + " AND " + "name=" + city.name)))
+			throw new NoSuchCityException(city.short);
+
+		executeQuery(delete("CITY", "name=" + city.name + " AND " + "idCity=" + city.short));
+	}
+
+	// ----- End Cities ------ //
+	
+	// ----- Begin Airports ------ //
+
+	def addAirport(data: Airport_data) = {
+		//Do query and shizzle
+		println("data: " + data);
+	}
+
+	// ----- End Airports ------ //
+	
+	def changeTemplatePricesTo(templ: Template, to: List[PricePeriod]) = {
+		//Check template..
+		checkTemplate(templ);
+
+		//Check price periods..
+	}
+
+	// ------ CHECKS ----- //
+
+	def checkTemplate(templ: Template) = {
+		//Check database to see if this template corresponds to a correct template
+		val query = "SELECT * FROM Templates WHERE flightnr=\"" + templ.flightnr + "\"";
+		println("Check template with query: " + query);
+
+		//If no such flightnr can be found..
+		throw new TemplateException(templ, "No template with such flightnr excists");
+	}
+
+	def evalGet(query: String): Boolean = {
+		//perform a get and return whether or not it was succesful.
+		return false;
+	}
+
+	def executeQuery(query: String) = {
+		//evaluta the query but don't return any result
+		println("query: " + query);
+	}
+
+	def select(select: String, from: String, where: String): String = {
+		return "SELECT " + select + " FROM " + from + " WHERE " + where;
+	}
+
+	def insert(into: String, values: String): String = {
+		return "INSERT INTO " + into + " VALUES " + values;
+	}
+
+	def update(update: String, set: String, where:String): String = {
+		return "UPDATE " + update + " SET " + set + " WHERE " + where;
+	} 
+
+	def delete(from:String, where:String) : String = {
+		return "DELETE FROM " + from + " WHERE "+ where;
+	}
+
 }
--- src/syntax/Types.scala
+++ src/syntax/Types.scala
@@ -5,60 +5,114 @@ package syntax
  */
 sealed abstract class Type;
 
+/**
+ * Implementation Kristof below
+ */
+
+/**
+ * Types for selecting and adding cities.
+ */
 sealed abstract class City extends Type;
-case class City1(name: String) extends City;
-case class City2(short: String) extends City;
+case class City1(name:String) extends City;
+case class City2(short:String) extends City;
+case class City3(name:String, short:String) extends City;
+case class City_data(name:String, short:String) extends Type;
 
-case class Template(flightnr: String) extends Type;
-
-case class Template_data(
-    flightnr: String,
-	from: City,
-	to: City,
-	periods: PricePeriod,
-	reccPeriod: String) extends Type;
-
-case class City_data(name: String, short: String) extends Type;
-
+/**
+ * Types for selecting and adding airports.
+ */
 sealed abstract class Airport extends Type;
-case class Airport1(name: String) extends Airport;
-case class Airport2(city: City) extends Airport;
-case class Airport3(short: String) extends Airport;
-
+case class Airport1(name:String) extends Airport;
+case class Airport2(city:City) extends Airport;
+case class Airport3(short:String) extends Airport;
 case class Airport_data(city:City, name:String, short:String) extends Type;
 
+/**
+ * Types for selecting and adding templates.
+ */
+case class Template(flightnr:String) extends Type;
+case class Template_data(
+    flightnr:String,
+	from:City,
+	to:City,
+	periods:PricePeriod,
+	airplaneType:AirplaneType,
+	reccPeriod: String) extends Type;
+
+/**
+ * Types for selecting and adding distances.
+ */
 case class Dist_data(from:City, to:City, dist:Int) extends Type;
+case class Dist(from:City, to:City) extends Type;
+/**
+ * Types for selecting and adding seattypes.
+ */
+case class SeatType(name:String) extends Type;
+case class SeatsWithType(nr:Int, typ:SeatType) extends Type;
+//for example SeatsWithType(50, SeatType("Business")) corresponds to 50 seats of business cards
+case class SeatType_data(name:String) extends Type;
 
-case class SeatType(name: String) extends Type;
+/**
+ * Types for selecting and adding airplanetypes.
+ */
+case class AirplaneType(name:String) extends Type;
+case class AirplaneType_data(name:String, arrangement:List[SeatsWithType]) extends Type;
 
-case class SeatsWithType(nr: Int, typ:SeatType) extends Type;
+/**
+ * Types for selecting and adding flight times.
+ */
+case class FlightTime_data(
+    from:City, 
+    to:City, 
+    airplaneType:AirplaneType, 
+    time:Int) extends Type;
+sealed abstract class FlightTime 
+case class FlightTime1(from:City, to:City) extends FlightTime;
+case class FlightTime2(from:City, to:City, airplaneType:AirplaneType) extends FlightTime;
 
-case class AirplainType_data(name: String, arrangement:List[SeatsWithType]) extends Type;
+/**
+ * Types for selecting periods.
+ */
+case class Period(from:String, to:String) extends Type;
 
-case class AirplainType(name: String) extends Type;
-
-case class Time_data(from:City, to:City, typ:AirplainType, time:Int) extends Type;
-
-case class Period(from: String, to:String) extends Type;
-
-
-
+/**
+ * Types for selecting and adding prices.
+ */
 sealed abstract class Prices extends Type;
 case class Prices1(price:Int, seats:List[Int]) extends Prices;
 case class Prices2(price:Int, typ:SeatType) extends Prices;
+case class PricePeriod(period:Period, prices:Prices) extends Type;
 
-case class PricePeriod(period: Period, prices: Prices) extends Type;
+/**
+ * Types for selecting and adding airline companies.
+ */
+sealed abstract class AirlineCompany extends Type;
+case class AirlineCompany1(name:String) extends AirlineCompany;
+case class AirlineCompany2(short:String) extends AirlineCompany;
+case class AirlineCompany_data(name:String, short:String) extends Type;
+
+
+
+/**
+ * Implementation Jonathan below
+ */
+
+/**
+ * Types for selecting booking slots.
+ */
+sealed abstract class BookingSlots extends Type;
+case class BookingSlot1(templates:List[Template], seat:List[Int], periods: List[Period]) extends BookingSlots;
+case class BookingSlot2(templates:List[Template], types:List[SeatType], periods: List[Period]) extends BookingSlots;
+case class BookingSlot3(flight:Flight, seat:Int);
+case class BookingSlot4(flight:Flight, typ:SeatType);
+
+/**
+ * Type for updating booking slots.
+ */
+sealed abstract class BookingSlotsUpdate extends Type;
+case class BookingSlotsUpdate1(price:Int) extends BookingSlotsUpdate;
 
 sealed abstract class Flight extends Type;
 case class Flight1(template:Template, period:Period);
 case class Flight2(fln:String);
 
-
-sealed abstract class BookingSlots extends Type;
-case class BookingSlot1(price:Int, templates:List[Template], seat:List[Int], periods: List[Period]) extends BookingSlots;
-case class BookingSlot2(price:Int, templates:List[Template], types:List[SeatType], periods: List[Period]) extends BookingSlots;
-case class BookingSlot3(price:Int, flight:Flight, seat:Int);
-case class BookingSlot4(price:Int, flight:Flight, typ:SeatType);
-
-sealed abstract class BookingSlotsUpdate extends Type;
-case class BookingSlotsUpdate1(price:Int) extends BookingSlotsUpdate;
