6911dfa7343e68091425f842587c58deca994c2f
dmerckx <davidmerckx01@hotmail.com>
2013-01-05 05:17:03 +0100

    vernieuwde parser

--- src/parser/Parser.scala
+++ src/parser/Parser.scala
@@ -1,20 +1,7 @@
 package parser
 
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
-import syntax.AddCity
-import syntax.Operation
-import syntax.Type
-import syntax.Opt
-import syntax.Empty
-import syntax.Filled
-import syntax.AddAirport
-import syntax.City
-import syntax.City
-import syntax.City1
-import syntax.City2
-import syntax.AddAirport
-import syntax.Airport_data
-import syntax.City_data
+import syntax._
 
 sealed abstract class ParseRes;
 case class PSucces(op:Operation) extends ParseRes;
@@ -22,31 +9,49 @@ case class PFail(msg:String) extends ParseRes;
 
 case class UnknownAttribute(name:String) extends Exception
 case class SkippedAttribute(name:String) extends Exception
+case class TooLittleAttributes() extends Exception
+case class DoubleAttribute() extends Exception
 
+//TODO: use custom Lexical instead of StdLexical
 object Parser extends StandardTokenParsers {
   type ParserType[T] = Parser[T];
   
-  lexical.delimiters += ("{", "}", ",", ":")
-  lexical.reserved += ("ADD","CITY", "AIRPORT", "hoihoi")
-
-  def parseOp(): Parser[Operation]  =
-    "ADD" ~> "CITY" ~> parseCityData ^^ {c => new AddCity(c)} |
-    "ADD" ~> "AIRPORT" ~> parseAirportData ^^ {a => new AddAirport(a)}
-            
- 
-  def parseAtts(parseAtt:Parser[(String,Any)]):Parser[Map[String,Any]] = {
-    parseAtt ~ "," ~ parseAtts(parseAtt) ^^ {case pair~","~map => map + pair} |
+  lexical.delimiters += ("{", "}", ",", ":", "[", "]")
+  lexical.reserved += ("ADD","CITY", "AIRPORT", "TO")
+  
+   /**
+   * Parse a list of attributes
+   */
+  def parseAtts(p:Parser[(String,Any)]):Parser[Map[String,Any]] =
+    "{" ~> parseAttsRec(p) <~ "}" ^^ {atts =>
+      if(atts.size == 0) throw new TooLittleAttributes()
+      atts}
+    
+  /**
+   * Parse a list of attributes recursively
+   */
+  def parseAttsRec(parseAtt:Parser[(String,Any)]):Parser[Map[String,Any]] = {
+    parseAtt ~ "," ~ parseAttsRec(parseAtt) ^^ {
+      case pair~","~map =>
+        if(map.contains(pair._1)) throw new DoubleAttribute()
+        map + pair} |
     parseAtt ^^ {pair => Map(pair)}}
   
-  def select[T](key:String, map:Map[String,Any]):T = {
+  /**
+   * Select a value from a map. Throws an error if the value could not be found.
+   */
+  def sel[T](key:String, map:Map[String,Any]):T = {
     println(" -- selects " + key + " from " + map);
     if(map.contains(key))
       map.get(key).get match {case t:T => t}
     else
-      throw new MatchError("hoi")
+      throw new MatchError("")
   }
-    
-  def selectOpt[T](key:String, map:Map[String,Any]):Opt[T] = {
+  
+  /**
+   * Select an optional value from a map.
+   */
+  def selOpt[T](key:String, map:Map[String,Any]):Opt[T] = {
     println(" -- selectOpt " + key + " from " + map);
     if(map.contains(key))
       map.get(key).get match {case s:T => Filled(s)}
@@ -54,43 +59,94 @@ object Parser extends StandardTokenParsers {
       Empty[T]
   }
   
+  /**
+   * Parse a type
+   */
+  def parseType[T <: Type](attName:String, typeParser:Parser[T]):Parser[(String, T)] =
+    ident ~ ":" ~ typeParser ^^ {
+    case attName~":"~typ => (attName, typ)}
+  
+  /**
+   * Parse a string
+   */
+  def parseStr(attName:String):Parser[(String, String)] =
+    ident ~ ":" ~ ident ^^ {
+    case attName~":"~str => (attName, str)} |
+    ident ~ ":" ~ stringLit ^^ {
+    case attName~":"~str => (attName, str)}
+  
+  def parseInt(attName:String):Parser[(String, Int)] =
+    ident ~ ":" ~ numericLit ^^ {
+    case attName~":"~int => (attName, int.toInt)}
+    
+  def parseIntList(attName:String):Parser[(String, List[Int])] = {
+    def par:Parser[List[Int]] = {
+      numericLit ~ "," ~ par ^^ {case int~","~rest => int.toInt :: rest} |
+      numericLit ^^ {case int => List(int.toInt)}
+    }
+    
+    ident <~ ":" <~ "[" <~ "]" ^^ {case attName => (attName, List())} |
+    ident ~ ":" ~ "[" ~ par ~ "]" ^^ {case attName~":"~"["~list~"]" => (attName, list)} 
+  }
+  
+  
+  // ----- ALL THE ACTUAL OPERATORS ----- //
+    
+  def parseOp(): Parser[Operation]  =
+    "ADD" ~> "CITY" ~> parseCityData ^^ {c => AddCity(c)} |
+    "CHANGE" ~> "CITY" ~> parseCity ~ "TO" ~ parseCity ^^ {case f~"TO"~t => ChangeCity(f, t)} |
+    "REMOVE" ~> "CITY" ~> parseCity ^^ {c => RemoveCity(c)} |
+    "ADD" ~> "AIRPORT" ~> parseAirportData ^^ {a => new AddAirport(a)} |
+    "CHANGE" ~> "AIRPORT" ~> parseAirport ~ "TO" ~ parseAirport ^^ {case f~"TO"~t => ChangeAirport(f,t)}
+    "REMOVE" ~> "AIRPORT" ~> parseAirport ^^ {a => new RemoveAirport(a)}
+    
+  // ----- ALL THE ACTUAL TYPES ----- //
+  
+  val cityAtts:Parser[(String,Any)] =
+    parseStr("name") | 
+    parseInt("id")
   val parseCity: Parser[City] =
-    "{" ~> parseAtts(city) <~ "}" ^^ {atts =>
-      if(!atts.contains("name")) City2(select("short",atts))
-      else City1(select("name",atts), selectOpt("short",atts))}
-  val city:Parser[(String,Any)] = {
-    ident ~ ":" ~ ident ^? {
-     case "name"~":"~name => ("name", name)
-     case "short"~":"~short => ("short", short)}}
-  
+    parseAtts(cityAtts) ^^ {as => City(selOpt[String]("name",as), selOpt[Int]("id",as))}
   val parseCityData: Parser[City_data] ={
-    "{" ~> parseAtts(cityData) <~ "}" ^^ {atts =>
-        println(atts);
-    	new City_data(select("name",atts), select("short",atts))}}
-  val cityData:Parser[(String,Any)] = {
-    ident ~ ":" ~ ident ^^ {
-     case "name"~":"~name => ("name", name)
-     case "short"~":"~short => ("short", short)
-     case name~":"~_ => throw new UnknownAttribute(name)}}
-
-  val parseAirportData: Parser[Airport_data] =
-    "{" ~> parseAtts(airportData) <~ "}" ^^ {atts =>
-        new Airport_data(select("city",atts), select("name",atts), select("short",atts))}
-  val airportData:Parser[(String,Any)] = 
-    ident ~ ":" ~ ident ^^ {
-     case "name"~":"~name => ("name", name)
-     case "short"~":"~short => ("short", short)
-     case name~":"~_ => throw new UnknownAttribute(name)} |
-    ident ~ ":" ~ parseCity ^^ {
-     case "city"~":"~city => ("city", city)
-     case name~":"~_ => throw new UnknownAttribute(name)} 
+    parseAtts(cityAtts) ^^ {as => new City_data(sel[String]("name",as), selOpt[Int]("short",as))}}
   
-  /*def parse (s: String) = {
+  val airportAtts:Parser[(String,Any)] = 
+    parseStr("name") |
+    parseStr("short") |
+    parseType("city", parseCity)
+  val parseAirport: Parser[Airport] =
+    parseAtts(airportAtts) ^^ {as => Airport(selOpt("name",as), selOpt("city",as), selOpt("short",as))}
+  val parseAirportData: Parser[Airport_data] =
+    parseAtts(airportAtts) ^^ {as => Airport_data(sel("city",as), sel("name",as), sel("short",as))}
+    
+  val prices1:Parser[(String,Any)] =
+    parseInt("price") |
+    parseIntList("seats") 
+  val prices2:Parser[(String,Any)] =
+    parseInt("price") 
+  val parsePrices: Parser[Prices] = 
+    parseAtts(prices1) ^? {case as => Prices1(sel("price",as), sel("seats", as))}
+    parseAtts(prices2) ^^ {as => Prices2(sel("price",as), sel("type", as))}
+  
+  
+  /**
+   * Used for tests
+   */
+  def parseType(s:String, typeName:String):Type ={
     val tokens = new lexical.Scanner(s)
     printTokens(tokens);
-    println(phrase(parseOp));
-    phrase(parseOp)(tokens)
-  }*/
+    
+    val parseFunc:Parser[Type] = typeName match {
+      case "city" => parseCity
+      case "cityData" => parseCityData
+      case "airport" => parseAirport
+      case "airportData" => parseAirportData
+      case "prices" => parsePrices
+    }
+    phrase(parseFunc)(tokens) match {
+      case Success(typ, _) => typ
+    }
+  } 
   
   def parse (s: String):ParseRes = {
     val tokens = new lexical.Scanner(s)
@@ -114,31 +170,9 @@ object Parser extends StandardTokenParsers {
       printTokens(sc.rest);
     }
   }
-  
-  /*def test (exprString : String) = {
-     try{
-       parse (exprString) match {
-         case Success(op, _) => println("Operation: " + op)
-         case Failure(msg, n) =>
-           println("Failure: " + msg + " next: " + n)
-           println("Tokens left");
-           printTokens(n.asInstanceOf[lexical.Scanner])
-         case Error(msg, _) => println("Error: " + msg)
-       }
-     }
-     catch{
-       case UnknownAttribute(n) => println("Unkown attribute used:" + n);
-       case SkippedAttribute(n) => println("Attribute was not provided:" + n)
-     }
-  }*/
 
   def main (args: Array[String]) = {
-    val x = 5
-    x match {
-      case 2 => 
-    }
     println("hello"); 
-    //test ("ADD AIRPORT {name:BRUSSELS, city:{short:deef}, short:BRU}")
   }   
 }
   
--- src/parser/Tests.scala
+++ src/parser/Tests.scala
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FunSuite
 import syntax.Operation
-import syntax._;
+import syntax.Type
+import syntax._
 
 @RunWith(classOf[JUnitRunner])
 class Tests extends FunSuite with ShouldMatchers {
@@ -23,6 +24,18 @@ class Tests extends FunSuite with ShouldMatchers {
         	println(outp.toString());
         	
         	if(!e.toString().equals(outp.toString()))
+        	  fail("expressions not equal");
+        }
+    }
+    
+    def parseTypeSucces(expr:String, typeName:String, outp: => Type) {
+    	test("parse type expression: " + expr + " <--> " + outp) {
+        	val out = outp
+            val t:Type = Parser.parseType(expr, typeName)
+        	println(t.toString());
+        	println(outp.toString());
+        	
+        	if(!t.toString().equals(outp.toString()))
         	  fail("expressions not equal");
         }
     }
@@ -45,28 +58,53 @@ class Tests extends FunSuite with ShouldMatchers {
    
    //-----Simple expression
     //Simple tests
-   parseSucces("ADD CITY {name:\"Brussels\", short:BRU}",
+   parseSucces("ADD CITY {name:Brussels}",
     			AddCity(City_data("Brussels", "BRU"))) 
-   parseSucces("ADD CITY {name:Malinas, short:MAL}",
-    			AddCity(City_data("Malinas", "MAL")))
     //Reverse order
    parseSucces("ADD CITY {short:MAL, name:Malinas}",
     			AddCity(City_data("Malinas", "MAL")))
+    //Parse with quotation marks
+   parseSucces("ADD CITY {short:\"MAL\", name:Malinas}",
+    			AddCity(City_data("Malinas", "MAL")))
+   parseSucces("ADD CITY {short:NY, name:\"New York\"}",
+    			AddCity(City_data("New York", "NY")))
     			
     			
    //-----Expression with nested type & optional parameters
     //Standard tests
    parseSucces("ADD AIRPORT {name:Brussels_Airport, city:{name:brussels, short:bru}, short:BRU}",
-    			AddAirport(Airport_data(City1("brussels",Filled("bru")),"Brussels_Airport","BRU")))
+    			AddAirport(Airport_data(City(Filled("brussels"),Filled("bru")),"Brussels_Airport","BRU")))
    parseSucces("ADD AIRPORT {name:Brussels_Airport, city:{short:bru}, short:BRU}",
-    			AddAirport(Airport_data(City2("bru"),"Brussels_Airport","BRU")))
+    			AddAirport(Airport_data(City(Empty(),Filled("bru")),"Brussels_Airport","BRU")))
     //Optional parameters unfilled
    parseSucces("ADD AIRPORT {name:Brussels_Airport, city:{name:brussels}, short:BRU}",
-    			AddAirport(Airport_data(City1("brussels",Empty()),"Brussels_Airport","BRU")))
+    			AddAirport(Airport_data(City(Filled("brussels"),Empty()),"Brussels_Airport","BRU")))
     //Reverse order
    parseSucces("ADD AIRPORT {city:{short:bru}, short:BRU, name:Brussels_Airport}",
-    			AddAirport(Airport_data(City2("bru"),"Brussels_Airport","BRU")))
+    			AddAirport(Airport_data(City(Empty(),Filled("bru")),"Brussels_Airport","BRU")))
    parseSucces("ADD AIRPORT {name:Brussels_Airport, short:BRU, city:{short:bru}}",
-    			AddAirport(Airport_data(City2("bru"),"Brussels_Airport","BRU")))
+    			AddAirport(Airport_data(City(Empty(),Filled("bru")),"Brussels_Airport","BRU")))
+    			
+   //-----Different operations-----
+    			
+   parseSucces("ADD CITY {name:Brussels}",
+    			AddCity(City_data("Brussels", Empty)))
+   parseSucces("ADD CITY {name:Springfield, id:5}",
+    			AddCity(City_data("Springfield", Filled(5))) 
+    			
+   //-----Different types------
+   
+   parseTypeSucces("{name:\"New York\"}", "city",
+		   		City(Filled("New York"), Empty()))
+   parseTypeSucces("{short:\"NY\"}", "city",
+		   		City(Empty(), Filled("NY")))
+   parseTypeSucces("{name:\"New York\", short:\"NY\"}", "cityData",
+		   		City_data("New York", "NY"))
+   parseTypeSucces("{name:\"Brussels airport\", city:{name:Brussels}, short:BRU}", "airport",
+		   		Airport(Filled("Brussels airport"), Filled(City(Filled("Brussels"), Empty())), Filled("BRU")))
+   parseTypeSucces("{name:\"Brussels airport\", city:{name:Brussels}, short:BRU}", "airportData",
+		   		Airport_data(City(Filled("Brussels"), Empty()), "Brussels airport", "BRU"))
+   parseTypeSucces("{price:100, seats:[1, 2]}", "prices",
+		   		Prices1(100, List(1, 2)))
     			
 }
